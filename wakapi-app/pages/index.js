3b5b236175995d49ef3e85373ec2106148015f97
rooespinoza <rocio.espinoza@alumnos.frm.utn.edu.ar>
2021-03-20 21:35:56 -0300

    Acomodo estilos reset.css

--- wakapi-app/pages/index.js
+++ wakapi-app/pages/index.js
@@ -1,10 +1,15 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import styles from './dashboard.module.scss'
+import Head from '../components/head'
 
 
 export const Dashboard = ({}) =>{
 return(
     <Fragment>
+      <Head title="Wakapi"></Head>
+      <div className={styles.dashboard__container}>
+        Hola!
+      </div>
     </Fragment>   
 )
 }
